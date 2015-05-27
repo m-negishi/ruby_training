cde975f392d03f07122d04573faae105634363f5
Masahiko.Negishi <negimann@yahoo.co.jp>
2015-05-27 15:41:42 +0900

    answer 7.6.2

--- TeachYourselfRuby/section7/7.6.2.rb
+++ TeachYourselfRuby/section7/7.6.2.rb
@@ -7,3 +7,32 @@ class Person
     @cleverness = cl
   end
 end
+
+class Fighter < Person
+  alias base_strength strength
+
+  def strength
+    @strength * 1.5
+  end
+end
+
+class Wizard < Person
+  alias base_strength strength
+  alias base_cleverness cleverness
+
+  def strength
+    @strength * 0.5
+  end
+
+  def cleverness
+    @cleverness * 3
+  end
+end
+
+f = Fighter.new(10, 10)
+puts f.strength
+puts f.base_strength
+#
+w = Wizard.new(10, 10)
+puts w.cleverness
+puts w.base_cleverness
