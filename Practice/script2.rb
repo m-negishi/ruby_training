8fc0e8eafd1d63ee467847bc0f0bb78a049daf5b
Masahiko.Negishi <negimann@yahoo.co.jp>
2015-05-27 14:34:53 +0900

    answer script2

--- Practice/script2.rb
+++ Practice/script2.rb
@@ -1,11 +1,6 @@
-# ワイルドカードはデフォルトではファイル名の先頭の "." にマッチしない
-# File::FNM_DOTMATCHフラグを第二引数に指定すればマッチする
-files_and_directories = Dir.glob('*', File::FNM_DOTMATCH)
-
-files = []
-
 pattern = /\d+.*\.(txt|html|css|md)$/
 
-files_and_directories.each { |f| files.push(f) if File.directory?(f) == false }
-
-files.each { |file| puts file if pattern =~ file }
+Dir.glob('*', File::FNM_DOTMATCH).each do |file|
+  next if File.directory?(file) == true
+  puts file if pattern =~ file
+end
