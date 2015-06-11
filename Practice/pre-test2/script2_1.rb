ab85c4fe81b5f7d78e8cb5f19a315988fffbf89a
Masahiko.Negishi <negimann@yahoo.co.jp>
2015-06-11 22:00:48 +0900

    answer issue 2-2

--- Practice/pre-test2/script2_1.rb
+++ Practice/pre-test2/script2_1.rb
@@ -6,51 +6,48 @@
 # 素因数分解したときに、素数が因数にない
 #
 # エラトステネスのふるい
-# Nが素数かどうかは、ルートNまでの整数で割ってみる
+# Nが素数かどうかは、Nの平方根までの整数で割ってみる
 
 include Math
 
 def prime_gen
   count = 1
-  prime_list = [2]
+  prime_list = []
+
   lambda do
     prefix = 0
-    # flag = 0
-    last_num = prime_list.last
-    # puts "count #{count}"
-    if last_num == 2
-      next_prime = 3
-      # puts "next_prime #{next_prime}"
-      prime_list << next_prime
+    last_prime = prime_list.last
+    # 2と3は例外的に処理
+    if last_prime.nil?
+      prime_list << 2
+    elsif last_prime == 2
+      prime_list << 3
     else
-      # puts "else"
       while prime_list.count < count
-        flag = 0
-        prefix += 1
-        # puts "prefix = #{prefix}"
-        next_prime = last_num + prefix
-        # puts "next_prime = #{next_prime}"
-        div = sqrt(next_prime).floor
-        # puts "div = #{div}"
-        # puts "next_prime.modulo(div) = #{next_prime.modulo(div)}"
-        (2..div).each do |n|
-          # puts "n = #{n}"
+        judge_prime = 0
+        # 2以外の偶数は素数ではないので奇数だけ生成
+        prefix += 2
+        next_prime = last_prime + prefix
+
+        (3..sqrt(next_prime).floor).each do |n|
           if next_prime.modulo(n) == 0
-            flag = 1
+            judge_prime = 1
+            # 一度でも割り切れたら素数ではない
             break
           end
         end
-        # puts "flag = #{flag}"
-        prime_list << next_prime if flag == 0
-        # puts "代入"
-        # prime_list << next_prime if next_prime.modulo(div) != 0
+
+        prime_list << next_prime if judge_prime == 0
       end
     end
+
     count += 1
-    p prime_list# << next_prime
+    p prime_list
   end
 end
 
+#
+#
 # 実行
 prime = prime_gen
 
@@ -58,10 +55,10 @@ prime = prime_gen
   prime.call
 end
 
-# 比較
+# 比較用
 require 'prime'
 
 generator = Prime::EratosthenesGenerator.new
 list = []
-Prime.each(71, generator) { |prime| list << prime }
+Prime.each(71, generator) { |prime_num| list << prime_num }
 p list
