33bb4e3378e3f4b925d3940ceb901931ad6f1753
Masahiko.Negishi <negimann@yahoo.co.jp>
2015-06-19 13:01:34 +0900

    finish 15.2

--- TeachYourselfRuby/section15/lib/date_range.rb
+++ TeachYourselfRuby/section15/lib/date_range.rb
@@ -1,7 +1,8 @@
 require 'date'
 
 class DateRange
-  attr_accessor :from, :to
+  attr_reader :from
+  attr_accessor :to
 
   def initialize(from, to)
     self.from = from
@@ -10,5 +11,10 @@ class DateRange
 
   def days
     (to - from).to_i + 1
+  end
+
+  def from=(from)
+    raise ArgumentError.new("from must be Date.") unless from.is_a? Date
+    @from = from
   end
 end
--- TeachYourselfRuby/section15/test/date_range_test.rb
+++ TeachYourselfRuby/section15/test/date_range_test.rb
@@ -5,12 +5,48 @@ require './lib/date_range'
 
 class DateRangeTest < Test::Unit::TestCase
 
-  def test_new
-    from = Date.new(2008, 9, 9)
-    to = Date.new(2008, 9, 10)
-    dr = DateRange.new(from, to)
+  # 各メソッドが実行される前に実行される
+  def setup
+    # puts 'setup method'
+    @from = Date.new(2008, 9, 9)
+    @to = Date.new(2008, 9, 10)
+    @dr = DateRange.new(@from, @to)
+  end
 
-    assert_equal from, dr.from
-    assert_equal to, dr.to
+  def test_from_should_be_date
+    # puts 'test_from_should_be_date'
+    # puts self.object_id
+    from = '2008/9/9'
+    to = Date.new(2008, 9, 10)
+    assert_raise(ArgumentError) do
+      dr = DateRange.new(from, to)
+    end
+  end
+
+  def test_from_should_be_date2
+    # puts 'test_from_should_be_date2'
+    # puts self.object_id
+    assert_raise(ArgumentError) do
+      @dr.from = "2008/9/9"
+    end
+  end
+
+  def test_new
+    # puts 'test_new'
+    assert_equal @from, @dr.from
+    assert_equal @to, @dr.to
+  end
+
+  def test_days_should_be_integer
+    assert_kind_of Integer, @dr.days
+  end
+
+  def test_days_should_be_two
+    assert_equal 2, @dr.days
+  end
+
+  # 各メソッドの実行後に毎回実行される
+  def teardown
+    # puts 'teardown'
   end
 end
