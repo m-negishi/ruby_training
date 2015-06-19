58b61b707aaca5cdd7840c3891c1a51fabde67ac
Masahiko.Negishi <negimann@yahoo.co.jp>
2015-06-19 19:58:40 +0900

    answer issue2-2 again

--- Practice/pre-test2/script2_2_2.rb
+++ Practice/pre-test2/script2_2_2.rb
@@ -37,106 +37,78 @@ class Priest < Person
   end
 end
 
+class Battle
+  attr_reader :relative_power_matrix
+
+  def initialize
+    @relative_power_matrix = {
+      'Fighter':
+        {
+          'Fighter': { st: 1.0, cl: 1.0 },
+          'Wizard': { st: 0.85, cl: 1.0 },
+          'Priest': { st: 1.0, cl: 1.0 }
+        },
+      'Wizard':
+        {
+          'Fighter': { st: 1.0, cl: 1.0 },
+          'Wizard': { st: 1.0, cl: 1.0 },
+          'Priest': { st: 1.0, cl: 0.75 }
+        },
+      'Priest':
+        {
+          'Fighter': { st: 0.95, cl: 0.9 },
+          'Wizard': { st: 1.0, cl: 1.0 },
+          'Priest': { st: 1.0, cl: 1.0 }
+        }
+    }
+  end
+
+  def result(player1, player2)
+    result = versus(player1, player2)
+    puts "#{player1.class}=#{result[:player1]} vs #{player2.class}=#{result[:player2]}"
+
+    if result[:player1] > result[:player2]
+      puts "#{player1.class}の勝ち"
+    elsif result[:player1] < result[:player2]
+      puts "#{player2.class}の勝ち"
+    else
+      puts '引き分け'
+    end
+  end
+
+  def versus(player1, player2)
+    power = {}
+
+    power[:player1] = calc_power(player1, relative_power_ratio(player1, player2))
+    power[:player2] = calc_power(player2, relative_power_ratio(player2, player1))
+    power
+  end
+
+  def calc_power(player, relative_power_ratio)
+    add_strength_and_cleverness(player, relative_power_ratio[:st], relative_power_ratio[:cl])
+  end
+
+  def add_strength_and_cleverness(player, st_ratio, cl_ratio)
+    player.strength * st_ratio + player.cleverness * cl_ratio
+  end
+
+  def relative_power_ratio(player1, player2)
+    @relative_power_matrix[:"#{player1.class}"][:"#{player2.class}"]
+  end
+end
+
+# 実行
 f = Fighter.new(10, 10)
 w = Wizard.new(10, 10)
 p = Priest.new(10, 10)
 
-
-# class Person
-#   attr_reader :strength
-#   attr_reader :cleverness
-#
-#   def initialize(st, cl)
-#     @strength = st
-#     @cleverness = cl
-#   end
-#
-#   def cal_power(obj=self, st_ratio=1.0, cl_ratio=1.0)
-#     obj.strength * st_ratio + obj.cleverness * cl_ratio
-#   end
-#
-#   def battle(enemy)
-#     result(cal_power, cal_power(enemy))
-#   end
-#
-#   def result(power, enemy_power, enemy)
-#     puts "#{self.class}:#{power} vs #{enemy.class}:#{enemy_power}"
-#     if power > enemy_power
-#       puts "#{self.class}の勝ち"
-#     elsif enemy_power > power
-#       puts "#{enemy.class}の勝ち"
-#     else
-#       puts "引き分け"
-#     end
-#   end
-# end
-#
-# class Fighter < Person
-#   alias base_strength strength
-#
-#   def strength
-#     @strength * 1.5
-#   end
-#
-#   def battle(enemy)
-#     if enemy.class.to_s == "Wizard"
-#       result(cal_power(self, 0.85), cal_power(enemy), enemy)
-#     elsif enemy.class.to_s == "Priest"
-#       result(cal_power, cal_power(enemy, 0.95, 0.9), enemy)
-#     else
-#       super
-#     end
-#   end
-# end
-#
-# class Wizard < Person
-#   alias base_strength strength
-#   alias base_cleverness cleverness
-#
-#   def strength
-#     @strength * 0.5
-#   end
-#
-#   def cleverness
-#     @cleverness * 3
-#   end
-#
-#   def battle(enemy)
-#     if enemy.class.to_s == "Priest"
-#       result(cal_power(self, 1.0, 0.75), cal_power(enemy), enemy)
-#     elsif enemy.class.to_s == "Fighter"
-#       result(cal_power, cal_power(enemy, 0.85), enemy)
-#     else
-#       super
-#     end
-#   end
-# end
-#
-# class Priest < Person
-#   alias base_cleverness cleverness
-#
-#   def cleverness
-#     @cleverness * 2
-#   end
-#
-#   def battle(enemy)
-#     if enemy.class.to_s == "Fighter"
-#       result(cal_power(self, 0.95, 0.9), cal_power(enemy), enemy)
-#     elsif enemy.class.to_s == "Wizard"
-#       result(cal_power, cal_power(enemy, 1.0, 0.75), enemy)
-#     else
-#       super
-#     end
-#   end
-# end
-#
-# f = Fighter.new(10, 10)
-# w = Wizard.new(10, 10)
-# p = Priest.new(10, 10)
-#
-# f.battle(w)
-# w.battle(f)
-# w.battle(p)
-# p.battle(w)
-# p.battle(f)
-# f.battle(p)
+battle = Battle.new
+battle.result(f, w)
+battle.result(w, f)
+battle.result(w, p)
+battle.result(p, w)
+battle.result(p, f)
+battle.result(f, p)
+battle.result(f, f)
+battle.result(w, w)
+battle.result(p, p)
